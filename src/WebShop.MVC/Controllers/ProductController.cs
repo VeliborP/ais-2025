53076421844428f3fb9e49b4e69e157d65e60aba
VeliborP <velci.aspire@gmail.com>
2025-12-08 12:53:11 +0100

    Update ProductController.cs

--- src/WebShop.MVC/Controllers/ProductController.cs
+++ src/WebShop.MVC/Controllers/ProductController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using WebShop.BLL.Interfaces;
-using WebShop.DAL.Models;
+//using WebShop.DAL.Models;
 using WebShop.MVC.ViewModels;
 
 namespace WebShop.MVC.Controllers
